7b9a22aa09d77522422048c973a6316cf9657568
Lisandris <lis170694@gmail.com>
2022-07-12 22:51:09 +0200

    creando listas

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
    
+   final options = const [' Megaman', 'Metal Gear', 'Super Smash', 'Final Fantasy'];
   const Listview1Screen({Key? key}) : super(key: key);
   
+ 
  @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +13,18 @@ class Listview1Screen extends StatelessWidget {
         title: const Text('Listview Tipo 1'),
       ),
       body: ListView(
-        children: const [
-          Text( "Hola Lis"),
+        children: [
+
+       
+        ...options.map(
+           (game) => ListTile(
+            title: Text(game),
+            trailing: const Icon( Icons.arrow_forward_ios_outlined), 
+           ) 
+       ).toList(),
+       
+          
+
         ],
       )
     );
