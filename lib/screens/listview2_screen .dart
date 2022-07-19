444c6a4456f6b2d9dc720ae89ebe0d28c99d1b50
Lisandris <lis170694@gmail.com>
2022-07-19 22:06:38 +0200

    aprendiendo estilos de appbar cambiando colores

--- lib/screens/listview2_screen .dart
+++ lib/screens/listview2_screen .dart
@@ -12,14 +12,20 @@ class Listview2Screen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Listview Tipo 2'),
+        elevation: 0,
+        backgroundColor: Colors.pink
       ),
       body: ListView.separated(
         itemCount: options.length,
         itemBuilder: (context, i) => ListTile(
           title: Text( options[i] ),
-          trailing: const Icon (Icons.arrow_forward_ios_outlined),
+          trailing: const Icon( Icons.arrow_forward_ios_outlined, color: Colors.pink, ),
+          onTap: () {
+            final game = options[i];
+            print( game );
+          },
         ), 
-        separatorBuilder: ( _, __ ) => const Divider(), 
+        separatorBuilder: ( _ , __ ) => const Divider(), 
       )
     );
   }
